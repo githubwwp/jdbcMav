ff03d3738c63d3988444340ea42192948be1df15
githubwwp <wwp231@163.com>
2018-06-13 16:51:35 +0800

    toolbar

--- src/main/webapp/jsp/module/js/toolbarModule.js
+++ src/main/webapp/jsp/module/js/toolbarModule.js
@@ -3,7 +3,18 @@ Ext.onReady(function() {
         tbar : [ {
             xtype : 'button',
             text : 'basic toolbar',
+            iconCls : 'icon-external-link icon-large icon-color-primary',
             handler : showBasicToolbar
+        }, {
+            xtype : 'button',
+            text : 'overflow toolbar',
+            iconCls : 'icon-tasks icon-large icon-color-primary',
+            handler : showOverflowToolbar
+        }, {
+            xtype : 'button',
+            text : 'status bar',
+            iconCls : 'icon-envelope icon-large icon-color-primary',
+            handler : showStatusBar
         } ]
     });
 
@@ -13,7 +24,7 @@ Ext.onReady(function() {
     });
 
     // TODO
-    showBasicToolbar();
+    showStatusBar();
 
 });
 
@@ -58,6 +69,76 @@ function showBasicToolbar() {
         width : 400,
         height : 400,
         title : 'basic toolbar',
+        modal : true,
         items : [ form ]
     });
+}
+
+function showOverflowToolbar() {
+
+    var form = Ext.create('Ext.form.Panel', {
+        tbar : {
+            layout : {
+                overflowHandler : 'Menu'
+            },
+            items : [ {
+                xtype : 'splitbutton',
+                text : 'munu button',
+                menu : [ {
+                    text : 'menu item 1'
+                } ]
+            }, {
+                xtype : 'splitbutton',
+                text : 'munu button',
+                menu : [ {
+                    text : 'menu item 2'
+                } ]
+            }, '-', {
+                xtype : 'splitbutton',
+                text : 'munu button',
+                menu : [ {
+                    text : 'menu item 3'
+                } ]
+            }, {
+                xtype : 'splitbutton',
+                text : 'munu button',
+                menu : [ {
+                    text : 'menu item 4'
+                } ]
+            }, '->', {
+                text : 'choose a color',
+                menu : [ {
+                    xtype : 'colorpicker'
+                } ]
+            } ]
+        }
+    });
+
+    Ext.create('Ext.window.Window', {
+        autoShow : true,
+        width : 200,
+        height : 400,
+        modal : true,
+        title : 'overflow toolbar',
+        layout : 'fit',
+        items : [ form ]
+    });
+}
+
+function showStatusBar() {
+
+    var form = Ext.create('Ext.form.Panel', {
+        tbar: []
+    });
+
+    Ext.create('Ext.window.Window', {
+        autoShow : true,
+        width : 400,
+        height : 400,
+        modal : true,
+        title : 'status bar',
+        layout : 'fit',
+        itmes : [ form ]
+    });
+
 }
