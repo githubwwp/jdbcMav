39491c9e8fd8717499e490a477e704e6b3a13622
githubwwp <wwp231@163.com>
2018-07-27 17:23:34 +0800

    nothing

--- src/main/webapp/formatJs.js
+++ src/main/webapp/formatJs.js
@@ -1,8 +1,9 @@
 for ( var i = 0; i < len; i++) {
     var rec = saleTaskStore.getAt(i);
-    for ( var j = 0; j < arrs.length; j++) {
-        if (Ext.isEmpty(arrs[0])) {
-            return "第" + (i + 1) + "行请输入" + arrs[1] + "！";
+    for ( var j = 0; j < vrs.length; j++) {
+        var vr = vrs[j];
+        if (Ext.isEmpty(rec.data[vr[0]])) {
+            return "第" + (i + 1) + "行请输入‘" + vr[1] + "’！";
         }
     }
 }
--- src/test/java/common/test/Test3.java
+++ src/test/java/common/test/Test3.java
@@ -8,18 +8,24 @@ import jdbc.util.FileUtil;
 public class Test3 {
 
     public static void main(String[] args) {
-        String fileName = "e:/z_temp/dbparse.txt";
-        String msg = FileUtil.readTextContent(fileName);
-        System.out.println(msg);
-
-        // 解析字符串
-        String pattern = "`[a-zA-Z0-9_]*`";
-        Pattern r = Pattern.compile(pattern);
-        Matcher m = r.matcher(msg);
-        while (m.find()) {
-            System.out.print(m.group() + ", ");
+//        String fileName = "abcdef123";
+//        String msg = FileUtil.readTextContent(fileName);
+//        System.out.println(msg);
+//
+//        // 解析字符串
+//        String pattern = "`[a-zA-Z0-9_]*`";
+//        Pattern r = Pattern.compile(pattern);
+//        Matcher m = r.matcher(msg);
+//        while (m.find()) {
+//            System.out.print(m.group() + ", ");
+//        }
+        
+        String str = "abcdeabc123abc!@#";
+        Pattern p = Pattern.compile("abc(?!de|12)");
+        Matcher m = p.matcher(str);
+        while(m.find()){
+            System.out.println(m.group());
         }
     }
-
 
 }
