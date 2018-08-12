f0629a7b734b67506012a5919145346b2fc3ac75
githubwwp <wwp231@163.com>
2018-08-12 23:40:27 +0800

    test4

--- src/test/java/common/test/Test2.java
+++ src/test/java/common/test/Test2.java
@@ -5,16 +5,21 @@ import java.util.regex.Pattern;
 
 public class Test2 {
     public static void main(String[] args) {
-//        String str = "wefew";
-//        Pattern p = Pattern.compile("e");
-//        Matcher m = p.matcher(str);
-//        
-//        while(m.find()){
-//            System.out.println(m.group());
-//        }
-        
-        System.out.println("?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?".split(",").length);
-        
+
+        String p = "(a)(b)(c)"; // 正则表达式
+        String str = "abcsefweljabcwef"; // 待匹配字符串
+        Pattern pattern = Pattern.compile(p);
+        Matcher matcher = pattern.matcher(str);
+        int count = matcher.groupCount(); // 获取分组计数
+
+        int n = 1;
+        // 依次查找匹配的字符串
+        while (matcher.find()) {
+            System.out.println("circle: " + n++);
+            for (int i = 0; i <= count; i++) {
+                System.out.println("\tgroup(" + i + "): " + matcher.group(i));
+            }
+        }
     }
 
 }
--- src/test/java/common/test/Test4.java
+++ src/test/java/common/test/Test4.java
@@ -1,12 +1,18 @@
 package common.test;
 
-import java.util.UUID;
-
 public class Test4 {
 
     public static void main(String[] args) {
-
-
+        // 替换字符或字符串，不能用正则表达式
+        System.out.println("abcabc".replace("ab", "t"));
+        // 替换第一个与给定正则表达式匹配的字符串
+        System.out.println("abcabc".replaceFirst("ab*", "t"));
+        // 替换所有与给定正则表达式匹配的字符串
+        System.out.println("abcabc".replaceAll("ab", "t"));
+        
     }
+    
+    
+
 
 }
