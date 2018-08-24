94c927ce714dbdf3b1c282ca268846020e7242bb
githubwwp <wwp231@163.com>
2018-08-24 17:24:35 +0800

    temp

--- src/main/webapp/formatJs.js
+++ src/main/webapp/formatJs.js
@@ -1,21 +1,11 @@
-if (v == '01') {
-    return '执行中';
-} else if (v == '02') {
-    return '预编号';
-} else if (v == '03') {
-    return '预-进行';
-} else if (v == '04') {
-    return '预-取消';
-} else if (v == '05') {
-    return '预-成功';
-} else if (v == '05') {
-    return '预-成功';
-} else if (v == '06') {
-    return '预-失败';
-} else if (v == '98') {
-    return '已撤销';
-} else if (v == '99') {
-    return '关闭';
-} else {
-    return '';
+// 合同年结
+function carryoverCont(){
+    var r = confirm("确认进行合同结转?");
+    if(r == true){
+        SalesContractRegisterDao.carryover(function(map){
+            alert(map.msg);
+        });
+    } else{
+        return;
+    }
 }
