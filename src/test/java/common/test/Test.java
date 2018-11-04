b5b77b6ec6a16557e52bbf6027ea012c0b3276e3
githubwwp <wwp231@163.com>
2018-11-04 13:46:00 +0800

    test.java

--- src/test/java/common/test/Test.java
+++ src/test/java/common/test/Test.java
@@ -2,13 +2,14 @@ package common.test;
 
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 public class Test {
 
 	public static void main(String[] args) {
 		Double d = 123235434234.0000007;
 
-		// 0 的用法 (不存在用0填充)
+		/*// 0 的用法 (不存在用0填充)
 		DecimalFormat d1 = new DecimalFormat("00000000000,000.000000");
 		d1.setRoundingMode(RoundingMode.HALF_UP);
 		System.out.println(d1.format(d));
@@ -27,8 +28,17 @@ public class Test {
 		
 		Double dou = 0.00;
 		System.out.println(dou);
-		System.out.println(0 == dou);
-		System.out.println(0 == 0.0);
+		System.out.println(0 == dou);*/
+		
+		NumberFormat numberInstance = NumberFormat.getNumberInstance();
+		numberInstance.setMaximumFractionDigits(2);
+		numberInstance.setMinimumFractionDigits(2);
+		numberInstance.setRoundingMode(RoundingMode.HALF_UP);
+		System.out.println(numberInstance.format(1.225));
+		System.out.println(numberInstance.format(1.224));
+		System.out.println(numberInstance.format(1.226));
+		System.out.println(numberInstance.format(1.235));
+		
 	}
 
 }
