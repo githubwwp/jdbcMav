a1437ca2039d3473133c948154ce130fdd6b3670
githubwwp <wwp231@163.com>
2019-04-11 21:05:06 +0800

    treePickter

--- src/main/webapp/html/js/store.js
+++ src/main/webapp/html/js/store.js
@@ -1,5 +1,49 @@
 Ext.onReady(function(){
 	
+	var treeStore = Ext.create('Ext.data.TreeStore', {
+        fields: [ 
+            'text', 'menuId', 'menuPid', 'menuName', 'menuUrl', 'menuRemark', 'menuOrder'
+        ],
+        proxy: {
+            type: 'ajax',
+            url: '/sysMenu/queryMenuVos.do',
+            reader: {
+                type: 'json',
+                root: 'sysMenuChildVo'
+            },
+            root: {
+            	text: '根节点',  
+	            id: '0',  
+	            expanded: true  
+            	
+            }
+        },
+        autoLoad: true
+    });
 	
+	var form = Ext.create('Ext.form.Panel', {
+		xtype: 'from',
+		title: 'form',
+		items: [
+			{
+				xtype: 'textfield',
+				fieldLabel: 'name'
+			},
+			{
+				fieldLabel: 'tree',
+				xtype: 'treepicker',
+				store: treeStore,
+				valueField: 'text',
+				displayField : 'text'
+			}
+		]
+	});
+	
+	new Ext.Viewport({
+		layout: 'fit',
+		items: [
+			form
+		]
+	});
 	
 });
--- src/test/java/common/test/Test.java
+++ src/test/java/common/test/Test.java
@@ -1,23 +1,9 @@
 package common.test;
 
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.FieldPosition;
-import java.text.ParseException;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.UUID;
-
 public class Test {
 
-	public static void main(String[] args) throws IOException, ParseException {
-		double d = 12;
-		System.out.println(3/2);
-		System.out.println(d/45);
-		
-		System.out.println(1*1/23);
-		
-		System.out.println(UUID.randomUUID().toString().replace("-", ""));
-	}
-
+    public static void main(String[] args) {
+        System.out.println(3/2);
+        System.out.println(1.0);
+    }
 }
