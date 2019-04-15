3463e53c38089e54c1c934873574a0639f9e0082
githubwwp <wwp231@163.com>
2019-04-15 22:05:18 +0800

    extjs-tree

--- src/main/webapp/html/js/baseTree.js
+++ src/main/webapp/html/js/baseTree.js
@@ -3,11 +3,24 @@ Ext.onReady(function() {
     var panel = Ext.create('Ext.panel.Panel', {
         width: '60%',
         height: '60%',
-        layout: 'fit',
+        layout: 'column',
+    	autoScroll: true,
         items: [
+	        {	
+	        	xtype: 'dcms_tree',
+	        	columnWidth: 0.5,
+	        	url: 'data/baseTree.json',
+	        	displayField: 'task',
+	        	storeFields: ['id', 'text', 'task'],
+	        	defaultLoad: true
+	        },
 	        {
-	        	xtype: 'yh-tree',
-	        	id: 'yh'
+	        	xtype: 'dcms_tree',
+	        	columnWidth: 0.5,
+	        	url: 'data/baseFlatData.json',
+	        	idConvertData: true,
+	        	storeFields: ['id', 'text', 'task'],
+	        	defaultLoad: true
 	        }
         ]
     });
@@ -21,22 +34,26 @@ Ext.onReady(function() {
 		items : panel
 	});
 	
-	// 加载
-	Ext.getCmp('yh').load();
 
 });
 
 
-// 定义树
+/*
+ * 自定义树组件 by wwp 2019-04-15
+ * url: 请求后台接口  (必填)
+ * storeFields: store 配置字段
+ * idDefaultLoad: 是否初始化加载(boolean 类型)
+ * idConvertData: 是否需要格式化数据 (boolean类型)
+ * 
+ */
 Ext.define('Ext.ux.baseTree', {
 	extend : 'Ext.tree.Panel',
-	xtype: 'yh-tree',
+	xtype: 'dcms_tree',
+	escapePid: '!@#',
+	
 	useArrows: true,
 	frame: true,
 	rootVisible: false,
-	title: 'base tree',
-	defaultRoot: {},
-	url: 'data/baseTree.json',
 	
 	initComponent: function(){
 		var me = this;
@@ -44,10 +61,16 @@ Ext.define('Ext.ux.baseTree', {
 		
 		// 配置
 		Ext.apply(me, {
+			autoLoad: false, // 强制 autoLoad 无效(测试设置为true 会报错)
 			store: new Ext.data.TreeStore({
-				root: {text: 'ro', id: -1}
+				fields: me.storeFields
 			})
 		});
+		
+		// 自动加载
+		if(me.defaultLoad){
+			me.load();
+		}
 		
 		me.callParent();
 	}, 
@@ -61,14 +84,73 @@ Ext.define('Ext.ux.baseTree', {
 	        method : 'post',
 	        success : function(rst) {
 	            var root = Ext.decode(rst.responseText);
+	            if(me.idConvertData){
+	            	root = me.convertDataFormat(root);
+	            }
+	            
 	            me.store.setRootNode(root);
 	        },
 	        failure : function(a) {
 	            alert(a.responseText);
 	        }
 	    });
+	},
+	// 转换数据格式
+	convertDataFormat: function(flatData){
+		var me = this;
+		var idArr = [];
+		var treeData = { text: 'root', id: me.escapePid };
+		
+		// 获取全部 id
+		for(var i=0, len=flatData.length; i< len; i++){
+			var obj = flatData[i];
+			idArr.push(obj.id);
+		}
+		
+		// 去重
+		Ext.Array.unique(idArr);
+		
+		//  获取需要展示的根节点, 将其根节点设置为 '!@#'
+		for(var i=0, len=flatData.length; i< len; i++){
+			var obj = flatData[i];
+			if(!Ext.Array.contains(idArr, obj.pid)){
+				obj.pid = me.escapePid;
+			}
+		}
+		
+		// 递归
+		me.getNodeChildren(treeData, flatData);
+		
+		return treeData;
+	},
+	// 取叶子节点
+	getNodeChildren: function(pObj, flatData) {
+	    var i = 0,
+	        j = 0,
+	        len = flatData.length;
+	
+	    // 遍历全部数据
+	    for (; i < len; i++) {
+	        var obj = flatData[i];
+	
+	        if (obj.pid == pObj.id) {
+	            pObj.children = pObj.children || [];
+	            pObj.children.push(obj);
+	
+	            this.getNodeChildren(obj, flatData);
+	        }
+	    }
+	
+	    // 设置叶子节点
+	    if (Ext.isEmpty(pObj.children)) {
+	        pObj.leaf = true;
+	    } else {
+	       	pObj.expanded = false;
+	    }
 	}
-})
+	
+});
+
 
 
 
